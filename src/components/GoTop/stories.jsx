a31d310d145f1cbb942cf5b959412eccc769fe9e
Rafael Noll <rafanoll2004@gmail.com>
2023-04-06 14:34:43 -0300

    Feature: Adding the component GoTop

---
+++ src/components/GoTop/stories.jsx
@@ -0,0 +1,39 @@
+import { GoTop } from ".";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    title: "GoTop",
+    component: GoTop,
+}
+
+export const Template = (args) => {
+    return (
+        <div style={{ height: '200vh' }}>
+            <h1>asdaiodhaosdihisa</h1>
+            <p>adsjhasgdsaghdfasg algydujfuyasfgdiuya iuiusag dgas dlya dsasdjasgdusadsa iuaghsdiguasiudsalk asgdasduljsagq</p>
+            <h1>asdaiodhaosdihisa</h1>
+            <p>adsjhasgdsaghdfasg algydujfuyasfgdiuya iuiusag dgas dlya dsasdjasgdusadsa iuaghsdiguasiudsalk asgdasduljsagq</p>
+            <h1>asdaiodhaosdihisa</h1>
+            <p>adsjhasgdsaghdfasg algydujfuyasfgdiuya iuiusag dgas dlya dsasdjasgdusadsa iuaghsdiguasiudsalk asgdasduljsagq</p>
+            <h1>asdaiodhaosdihisa</h1>
+            <p>adsjhasgdsaghdfasg algydujfuyasfgdiuya iuiusag dgas dlya dsasdjasgdusadsa iuaghsdiguasiudsalk asgdasduljsagq</p>
+            <h1>asdaiodhaosdihisa</h1>
+            <p>adsjhasgdsaghdfasg algydujfuyasfgdiuya iuiusag dgas dlya dsasdjasgdusadsa iuaghsdiguasiudsalk asgdasduljsagq</p>
+            <h1>asdaiodhaosdihisa</h1>
+            <p>adsjhasgdsaghdfasg algydujfuyasfgdiuya iuiusag dgas dlya dsasdjasgdusadsa iuaghsdiguasiudsalk asgdasduljsagq</p>
+            <h1>asdaiodhaosdihisa</h1>
+            <p>adsjhasgdsaghdfasg algydujfuyasfgdiuya iuiusag dgas dlya dsasdjasgdusadsa iuaghsdiguasiudsalk asgdasduljsagq</p>
+            <h1>asdaiodhaosdihisa</h1>
+            <p>adsjhasgdsaghdfasg algydujfuyasfgdiuya iuiusag dgas dlya dsasdjasgdusadsa iuaghsdiguasiudsalk asgdasduljsagq</p>
+            <h1>asdaiodhaosdihisa</h1>
+            <p>adsjhasgdsaghdfasg algydujfuyasfgdiuya iuiusag dgas dlya dsasdjasgdusadsa iuaghsdiguasiudsalk asgdasduljsagq</p>
+            <h1>asdaiodhaosdihisa</h1>
+            <p>adsjhasgdsaghdfasg algydujfuyasfgdiuya iuiusag dgas dlya dsasdjasgdusadsa iuaghsdiguasiudsalk asgdasduljsagq</p>
+            <h1>asdaiodhaosdihisa</h1>
+            <p>adsjhasgdsaghdfasg algydujfuyasfgdiuya iuiusag dgas dlya dsasdjasgdusadsa iuaghsdiguasiudsalk asgdasduljsagq</p>
+            <h1>asdaiodhaosdihisa</h1>
+            <p>adsjhasgdsaghdfasg algydujfuyasfgdiuya iuiusag dgas dlya dsasdjasgdusadsa iuaghsdiguasiudsalk asgdasduljsagq</p>
+            <GoTop {...args} />
+        </div>
+    )
+}
