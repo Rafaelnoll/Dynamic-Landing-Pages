5eb6ad2ee268e94291e5e7943ea0c97e8a6cfb69
Rafael Noll <rafanoll2004@gmail.com>
2023-04-13 16:03:51 -0300

    Feature: Loading the datas from API

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,8 +1,41 @@
+import { useState } from "react";
 import { Base } from "../Base";
 import mockBase from '../Base/mock';
 import * as Styled from './styles';
+import { mapData } from "../../api/map-data";
+import { useEffect } from "react";
 
 function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      // eslint-disable-next-line no-restricted-globals
+      const pathName = location.pathname.replace(/[^a-z0-9-_]/gi, '');
+      const slug = pathName ? pathName : 'landing-page';
+
+      try {
+        const data = await fetch(`http://localhost:1337/api/pages/?filters[slug]=${slug}&populate=deep`)
+        const json = await data.json();
+        const { attributes } = json.data[0];
+        const pageData = mapData([attributes]);
+        setData(() => pageData[0]);
+      } catch {
+        setData(undefined);
+      }
+    }
+
+    loadData();
+  }, []);
+
+  if (data === undefined) {
+    return <h1>Page not found</h1>
+  }
+
+  if (data && !data.slug) {
+    return <h1>Carregando...</h1>
+  }
+
   return (
     <Styled.Wrapper>
       <Base {...mockBase} />
