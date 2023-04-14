3c3d649d4655677780e45e5df5134d7c2e35769c
Rafael Noll <rafanoll2004@gmail.com>
2023-04-14 15:41:42 -0300

    Fix: SectionBackground randon id

--- src/components/SectionBackground/index.jsx
+++ src/components/SectionBackground/index.jsx
@@ -3,10 +3,8 @@ import * as Styles from './styles';
 import P from 'prop-types';
 import { SectionContainer } from "../SectionContainer";
 
-const randonId = () => `id-${Math.random() * 10000}`.replace(/[^a-z0-9-_]/gi);
-
 export const SectionBackground = ({ children, background = false, sectionId = '' }) => {
-    const id = sectionId ? sectionId : randonId();
+    const id = sectionId ? sectionId : '';
 
     return (
         <Styles.Container background={background} id={id}>
