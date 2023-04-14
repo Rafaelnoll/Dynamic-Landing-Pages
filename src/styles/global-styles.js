abcae8454d17ca97538dc1a29ed00f43faaa0fb3
Rafael Noll <rafanoll2004@gmail.com>
2023-04-14 14:57:29 -0300

    Feature: Adding table styles

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -34,4 +34,33 @@ export const GlobalStyles = createGlobalStyle`
     a{
         color: ${({ theme }) => theme.colors.secondaryColor};
     }
+
+    .table-content{
+        width:100%;
+        overflow-y: auto;
+        margin-top: ${({ theme }) => theme.spacings.medium};
+
+        table{
+            width:100%;
+
+            caption{
+                font-size:  ${({ theme }) => theme.font.sizes.small};
+                font-weight: bold;
+                text-align:left;
+                margin-bottom: ${({ theme }) => theme.spacings.xsmall};
+            }
+
+            thead,tfoot{
+                background-color: ${({ theme }) => theme.colors.mediumGray};
+            }
+
+            th, td{
+                padding: ${({ theme }) => theme.spacings.xsmall};
+                text-align: left;
+                border: 0.1rem solid ${({ theme }) => theme.colors.mediumGray};
+                font-size: 2rem;
+                white-space: nowrap;
+            }
+        }
+    }
 `;
